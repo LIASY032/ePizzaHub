9509f041d82fb7afcb7e4389093ce4a5e9c613ce
LIASY032 <liasy032@mymail.unisa.edu.au>
2022-07-25 21:27:10 +0930

    all entities added

--- ePizzaHubEntities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ePizzaHubEntities
-{
-    public class Class1
-    {
-    }
-}
---
+++ ePizzaHubEntities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ePizzaHub.Entities
+{
+    public class User: IdentityUser<int>
+    {
+        public string Name { get; set; }
+
+        [NotMapped]
+        public string[] Roles { get; set; }
+    }
+}
